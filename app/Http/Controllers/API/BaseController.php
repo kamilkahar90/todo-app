26947b9f4ee3cba63a800fcda53214195977984d
kamilkahar90 <kamilkahar90@gmail.com>
2022-04-24 13:22:12 +0800

    Added -install sanctum -base controller -register controller -modified user model and migration -login and register api routing

---
+++ app/Http/Controllers/API/BaseController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BaseController extends Controller
+{
+    public function returnSuccess($result, $message)
+    {
+    	$response = [
+            'success' => true,
+            'data'    => $result,
+            'message' => $message,
+        ];
+
+
+        return response()->json($response, 200);
+    }
+
+    public function returnError($error, $errorMessages = [], $code = 404)
+    {
+    	$response = [
+            'success' => false,
+            'message' => $error,
+        ];
+
+        if(!empty($errorMessages)){
+            $response['data'] = $errorMessages;
+        }
+
+        return response()->json($response, $code);
+    }
+}
